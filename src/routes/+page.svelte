085ebcbc2e32a306f4f0e03178823d6a9d2e3ff1
Godwin Jemegah <godwinjdayo@gmail.com>
2024-10-25 16:11:47 +0100

    added searchbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
-content
+<form class="flex items-center w-full max-w-full flex-col gap-6">
+  <div class="relative w-full">
+    <input
+      type="text"
+      placeholder="Enter prompt here"
+      class="w-full py-5 px-4 pl-4 pr-10  bg-bg-100 border border-foreground rounded-lg focus:outline-none focus:border-black-100 focus:border-transparent"
+    />
+    <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+        <button
+        type="submit"
+        class="w-32 h-11 font-semibold text-white bg-black rounded-lg hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center justify-center
+        hover:text-white transition-colors
+        "
+      >
+        Search
+      </button>
+      </div>
+  </div>
+ 
+</form>
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,9 @@ export default {
 			colors: {
 				"black-100": '#1F1F1F',
 				"black-200": '#333333',
-				"black-300": "#5e5e5e"
+				"black-300": "#5e5e5e",
+				"bg-100": "#FAFAFA",
+				"bg-200": "#EFEFEF"
 			},
 			fontFamily: {
 				inter: ['Inter', 'sans-serif'],
